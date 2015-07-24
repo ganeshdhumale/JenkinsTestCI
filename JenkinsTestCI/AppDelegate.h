8735c7ea4ba5e631316ffcad05c965e9866fc68e
GD <dhumaleganesh@gmail.com>
2015-07-24 13:17:32 +0530

    - Added new code base to git JenkinsTestCI.

---
+++ JenkinsTestCI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsTestCI
+//
+//  Created by GD on 24/07/15.
+//  Copyright (c) 2015 Extentia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenkinsTestCI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsTestCI
+//
+//  Created by GD on 24/07/15.
+//  Copyright (c) 2015 Extentia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
